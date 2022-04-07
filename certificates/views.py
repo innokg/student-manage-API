0fd4aace929bfbf0ba95fb01b87d1451c964fdee
Ulan Chokushov <ulan.chokushov@gmail.com>
2022-04-07 12:50:32 +0600

    added new app Lecture

--- certificates/views.py
+++ certificates/views.py
@@ -9,3 +9,7 @@ class CertificateSerializer(serializers.ModelSerializer):
     class Meta:
         model = Certificate
         fields = ('id', 'name', 'description', 'created_at', 'updated_at')
+
+class CertificateViewSet(viewsets.ModelViewSet):
+    queryset = Certificate.objects.all()
+    serializer_class = CertificateSerializer
--- eduPlatformAPI/settings.py
+++ eduPlatformAPI/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'rest_framework',
     'users',
     'certificates',
+    'waitlist',
+    'lectures',
 ]
 
 MIDDLEWARE = [
